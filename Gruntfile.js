85c43334d87ce3e52f890202a9d874518982d08f
Bradley C Bailey <bcbailey@godaddy.com>
2014-09-02 15:03:52 -0600

    Added dev watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@
 
 module.exports = function(grunt) {
   grunt.initConfig({
+    pkg: require('./package.json'),
+
     jshint: {
       options: {
         jshintrc: true
@@ -9,11 +11,35 @@ module.exports = function(grunt) {
       build: [ 'Gruntfile.js' ],
       source: [ 'tasks/**/*.js' ],
       test: [ 'test/**/*.js' ]
+    },
+
+    watch: {
+      jshintrc: {
+        files: [ '.jshintrc' ],
+        tasks: [ 'jshint' ]
+      },
+
+      build: {
+        files: [ '<%= jshint.build %>' ],
+        tasks: [ 'jshint:build' ]
+      },
+
+      source: {
+        files: [ '<%= jshint.source %>' ],
+        tasks: [ 'jshint:source', 'test' ]
+      },
+
+      test: {
+        files: [ '<%= jshint.test %>' ],
+        tasks: [ 'jshint:test', 'test' ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
+  grunt.registerTask('dev',  [ 'lint', 'watch' ]);
   grunt.registerTask('lint', [ 'jshint' ]);
   grunt.registerTask('test', [ ]);
 
